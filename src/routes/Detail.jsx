360b43fe92686680273462ce59b38656591fa1ae
Yoonju Ryu <uuj92@naver.com>
2022-06-08 23:07:14 +0900

    22-06-08

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,18 +1,25 @@
 import { useParams } from "react-router";
-import React from "react";
-import { axios } from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Detail() {
   const { id } = useParams();
+  const [test, setTest] = useState();
   useEffect(() => {
-    const json = await axios.get(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
-    console.log(json);
-    
+    async function fetchData() {
+      const json = await axios.get(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      );
+      console.log(json);
+      setTest(json);
+    }
+    fetchData();
   }, []);
   return (
     <div>
       <div>Detail</div>
       <p>어쩌구 저쩌구 왜 안될까요</p>
+      {JSON.stringify(test)}
     </div>
   );
 }
